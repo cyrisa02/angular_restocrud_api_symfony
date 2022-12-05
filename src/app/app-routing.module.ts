372a342845e6b53e60c5c2969980e0674c3f21ac
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-12-05 15:43:39 +0100

    Add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRestoComponent } from './add-resto/add-resto.component';
 import { EditRestoComponent } from './edit-resto/edit-resto.component';
+import { HomeComponent } from './home/home.component';
 import { RestoitemComponent } from './restoitem/restoitem.component';
 
 const routes: Routes = [
   { path: "resto", component: RestoitemComponent },
   { path: 'add-resto', component: AddRestoComponent },
   { path: 'edit-restaurant/:id', component: EditRestoComponent },
+  { path: "", component: HomeComponent },
 
 ];
 
